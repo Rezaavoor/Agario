6fd40c7ee2b50f340b3bd8ecf372e8f823c7be0a
Rezaavoor <Reza.avoor@gmail.com>
2022-02-23 21:37:09 +0100

    add some game logic + add Timer3

--- mipslabwork.c
+++ mipslabwork.c
@@ -15,23 +15,14 @@
 #include "mipslab.h"  /* Declatations for these labs */
 
 int mytime = 0x4557;
-int timeoutcount = 0;
+int timeoutcount2 = 0;
+int timeoutcount3 = 0;
 
 char textstring[] = "text, more text, and even more text!";
 
-void setupScreen(){
+// project stuff
 
-  display_string(0, "");
-  display_string(1, "");
-  display_string(2, "");
-  display_string(3, "");
-  display_update();
 
-  int i = 0;
-  for(i = 0; i<512;i++){
-    screen[i] = 255;
-  }
-}
 
 /* Interrupt Service Routine */
 void user_isr( void )
@@ -41,23 +32,41 @@ void user_isr( void )
 
 /* Lab-specific initialization goes here */
 
-volatile int* trisE = (volatile int*) 0xbf886100; //  =TRISE
 void labinit( void )
 {
-  *trisE &= 0xffffff00; //PORTE = output
-
+  TRISE &= 0xffffff00; //PORTE = output
+  TRISF &= 0x2; //  0000 0010 => BTN1 = output
   TRISD |= 0x0FE0; // 0000 1111 1110 0000     bits 5-11 are inputs
 
 
+  //Timer 2
   T2CON = 0x70; // TCKPS = 111 =>  0111 0000 bit 4-6 are all 1 => prescaling 1:256
   T2CON &= 0xFD; // TCS = 0 => bit 1 is 0 => internal peripheral clock gets used
 
-  PR2 = (80000000/256)/10; // 80MHZ / 256 presacling = 1s    =>     ans/10 = 100ms
+  PR2 = (80000000/256)/100; // 80MHZ / 256 presacling = 1s    =>     ans/10 = 10ms
   TMR2 = 0; // timer starts at 0
 
   T2CON |= 0x8000; // ON = 1 => bit 15 is 1 => timer is enabled
 
+
+  //Timer 3
+  T3CON = 0x70; // TCKPS = 111 =>  0111 0000 bit 4-6 are all 1 => prescaling 1:256
+  T3CON &= 0xFD; // TCS = 0 => bit 1 is 0 => internal peripheral clock gets used
+
+  PR3 = (80000000/256)/100; // 80MHZ / 256 presacling = 1s    =>     ans/10 = 10ms
+  TMR3 = 0; // timer starts at 0
+
+  T3CON |= 0x8000; // ON = 1 => bit 15 is 1 => timer is enabled
+
   setupScreen();
+  markRect(20, 5, 7);
+  markRect(100, 15, 1);
+  markRect(50, 1, 1);
+  //markRect(110, 30, 1);
+  //markRect(46, 2, 2);
+  //markRect(70, 8, 10);
+
+  createAgario();
 
   return;
 }
@@ -69,7 +78,7 @@ int counter = 0x00;
 
 
 
-int x=0,y=0;
+int x=5,y=0;
 void labwork( void )
 {
   
@@ -89,47 +98,51 @@ void labwork( void )
         mytime += swData*16;  // ex: swData=4 => firstDigit = 0x0040 
   }*/
 
+  if((IFS(0)>>12) & 1){
+    IFS(0) &= 0xffffefff;
+    timeoutcount3++;
+    if(timeoutcount3==5){
+      int btnData = getbtns();
+
+      if(btnData & 8){  // 1000
+        moveAgario(-1, 0);
+      }
+      if(btnData & 4){  // 0100
+        moveAgario(0, 1);
+      }
+      if(btnData & 2){  // 0010
+        moveAgario(0, -1);
+      }
+      if(btnData & 1){  // 0001
+        moveAgario(1, 0);
+      }
+      timeoutcount3 = 0;
+    }
+  }
+
   if((IFS(0)>>8) & 1)  //   bit 8 is being masked,  if bit 8 = 1 means timeout
   {
 
 
-    IFS(0) = 0x0; // reset the flag
-    timeoutcount++;
-    if(timeoutcount==2){
-      //time2string( textstring, mytime );
-      //display_string( 3, textstring );
-      //tick( &mytime );
-
+    IFS(0) &= 0xfffffeff; // 1110 1111 1111 reset the bit 8
+    timeoutcount2++;
+    if(timeoutcount2==8){
+      //LEDs shining
       *portE = counter;
-      counter += 0x1; // +1
+      counter += 0x1; // +1   
+    
+      checkHit();
 
-
-      ////////////////////////////////////////
-      
-      if(y<32){
-        if(x<128){
-          markPixel(x,y);
-          x++;
-        }
-        else{
-          x=0;
-          y++;
-        }
-      }
-      else{
-        y=0;x=0;
-        int i = 0;
-        for(i = 0; i<512;i++){
-          screen[i] = 255;
-        }
-      }
-      markPixel(x,y);
+      moveRect(0, -1, 0);
+      moveRect(1, 1, 0);
+      moveRect(2, 4, 2);
+      markAgario();
       display_image(0, screen);
 
       ////////////////////////////////////////
 
 
-      timeoutcount = 0;
+      timeoutcount2 = 0;
 
     }
   }
